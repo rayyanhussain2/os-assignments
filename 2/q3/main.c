3eb26ad11347d33817e61d23b6ec351865db0e65
Rayyan Hussain <rayyanhussain7@protonmail.com>
2023-09-16 15:47:31 +0530

    q2

--- 2/q3/main.c
+++ 2/q3/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/resource.h>
 #include <linux/sched.h>
+#include <linux/syscalls.h>
 
 int main(){
-
-
-    return 0;
+    struct task_struct *task;
+    for_each_process(task) {
+        printk(KERN_INFO "Process ID: %d, Command: %s\n", task->pid, task->comm);
+    }
+    
+    kernel_headers
+    
 }
